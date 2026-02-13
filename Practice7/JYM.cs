30e1e6e0070c7de0dfb7e24de50d379b1c307e11
Алексей <alexeikraichik@yandex.ru>
2026-02-13 14:43:37 +0300

    Generic JYM

---
+++ Practice7/JYM.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace JYM
+{
+    public class Gym<T>
+    {
+        public T[] data;
+        public int count;
+        public int capacity;
+
+        public Gym()
+        {
+            capacity = 4;
+            data = new T[capacity];
+            count = 0;
+        }
+
+        public int Count()
+        {
+            return count;
+        }
+
+        public void Double()
+        {
+            capacity = capacity * 2;
+            T[] newData = new T[capacity];
+            for (int i = 0; i < count; i++)
+            {
+                newData[i] = data[i];
+            }
+            data = newData;
+        }
+
+        public void Add(T item)
+        {
+            if (count >= capacity)
+            {
+                Double();
+            }
+            data[count] = item;
+            count++;
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= count)
+                {
+                    throw new IndexOutOfRangeException("Индекс не тот");
+                }
+                return data[index];
+            }
+            set
+            {
+                if (index < 0 || index >= count)
+                {
+                    throw new IndexOutOfRangeException("Индекс не тот");
+                }
+                data[index] = value;
+            }
+        }
+
+        public void RemoveAt(int index)
+        {
+            if (index < 0 || index >= count)
+            {
+                throw new IndexOutOfRangeException("Индекс не тот");
+            }
+
+            for (int i = index; i < count - 1; i++)
+            {
+                data[i] = data[i + 1];
+            }
+        }
+
+        public void Insert(int index, T item)
+        {
+            if (index < 0 || index >= count)
+            {
+                throw new IndexOutOfRangeException("Индекс не тот");
+            }
+
+            if (count >= capacity)
+            {
+                Double();
+            }
+
+            data[index] = item;
+            count++;
+        }
+    }
+}
+
---
+++ Practice7/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using JYM;
+
+class Program
+{
+    static void Main()
+    {
+        Gym<string> gym = new Gym<string>();
+        for (int i = 0; i < 1000; i++)
+        {
+            gym.Add($"Тренажёр номер {i}");
+        }
+        Console.WriteLine(gym[0]);
+        Console.WriteLine(gym[500]);
+        Console.WriteLine(gym[999]);
+        gym.RemoveAt(0);
+        gym.Insert(500, "Новое что то");
+        Console.WriteLine(gym[500]);
+        Console.WriteLine(gym[0]);
+    }
+}
